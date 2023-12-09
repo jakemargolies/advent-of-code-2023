df362e54267b0c683721fa8ccf00b065acc5d970
Jake Margolies <jacob.margolies@gtri.gatech.edu>
2023-12-09 17:50:28 +0000

    Finished Day 5: Part 2

--- day5/include/day5.hpp
+++ day5/include/day5.hpp
@@ -1,26 +1,59 @@
 #include <list>
 #include <string>
 
+class KeyRangeEntry
+{
+private:
+    long min;
+    long max;
+    long length;
+
+public:
+    KeyRangeEntry();
+    KeyRangeEntry(long range_min, long range_length);
+    long get_min() const;
+    long get_max() const;
+};
+
 class MapRangeEntry
 {
 private:
+    long src_max;
+
+protected:
     long length;
-    long src_start;
-    long src_end;
-    long dst_start;
+    long src_min;
+    long dst_min;
 
 public:
     MapRangeEntry(std::string map_entry_line);
 
+    MapRangeEntry();
+
     bool contains(long key) const;
 
+    bool contains(KeyRangeEntry key_range_entry) const;
+
     long get_mapped_value(long key) const;
+
+    KeyRangeEntry get_mapped_range(KeyRangeEntry key_range_entry, std::list<KeyRangeEntry>& leftover_key_ranges) const;
 };
 
-class Map : std::list<MapRangeEntry>
+
+class Map : public std::list<MapRangeEntry>
 {
 public:
-    long &operator[](long key);
+    long operator[](long key) const;
+
+    std::list<KeyRangeEntry> operator[](KeyRangeEntry key_range_entry) const;
+};
+
+class SeedRangeList : public std::list<KeyRangeEntry>
+{
+public:
+    SeedRangeList(std::string seeds_line);
 };
 
 void parse_seeds(std::list<long> *seeds, std::string seeds_line);
+
+void parse_and_interpolate_seeds(std::list<long> *seeds, std::string seeds_line);
--- day5/main.cpp
+++ day5/main.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <fstream>
 #include <iostream>
 #include <list>
@@ -7,31 +8,130 @@
 
 #define INPUT_FILENAME "almanac.txt"
 
+std::ifstream input_file(INPUT_FILENAME);
+std::string line;
+std::list<long> seeds;
+
+Map seed_to_soil_map;
+Map soil_to_fertilizer_map;
+Map fertilizer_to_water_map;
+Map water_to_light_map;
+Map light_to_temp_map;
+Map temp_to_hum_map;
+Map hum_to_location_map;
+
+std::list<Map> maps = {
+    seed_to_soil_map,
+    soil_to_fertilizer_map,
+    fertilizer_to_water_map,
+    water_to_light_map,
+    light_to_temp_map,
+    temp_to_hum_map,
+    hum_to_location_map};
+
+int part_1()
+{
+    // Extract seeds into list
+    std::string seeds_line;
+    std::getline(input_file, seeds_line);
+    parse_seeds(&seeds, seeds_line);
+
+    // Strip empty line
+    std::getline(input_file, line);
+
+    for (Map &map : maps)
+    {
+        // Strip header
+        std::getline(input_file, line);
+
+        // Populate map
+        while (std::getline(input_file, line) && !line.empty())
+        {
+            MapRangeEntry entry(line);
+            map.push_back(entry);
+        }
+    }
+
+    long min_location = LONG_MAX;
+    for (long seed : seeds)
+    {
+        long key = seed;
+        for (Map &map : maps)
+        {
+            key = map[key];
+        }
+        long location = key;
+        if (location < min_location)
+        {
+            min_location = location;
+        }
+    }
+
+    std::cout << min_location << std::endl;
+
+    return 0;
+}
+
+int part_2()
+{
+    // Extract seeds into list
+    std::string seeds_line;
+    std::getline(input_file, seeds_line);
+    SeedRangeList seed_ranges(seeds_line);
+
+    // Strip empty line
+    std::getline(input_file, line);
+
+    for (Map &map : maps)
+    {
+        // Strip header
+        std::getline(input_file, line);
+
+        // Populate map
+        while (std::getline(input_file, line) && !line.empty())
+        {
+            MapRangeEntry entry(line);
+            map.push_back(entry);
+        }
+    }
+
+    long min_location = LONG_MAX;
+
+    std::list<KeyRangeEntry> key_ranges = seed_ranges;
+    for (Map &map : maps)
+    {
+        std::list<KeyRangeEntry> dst_ranges;
+        std::list<KeyRangeEntry> new_key_ranges;
+        while (!key_ranges.empty())
+        {
+            KeyRangeEntry key_range = key_ranges.back();
+            key_ranges.pop_back();
+            dst_ranges = map[key_range];
+            for (KeyRangeEntry dst_range : dst_ranges)
+            {
+                new_key_ranges.push_back(dst_range);
+            }
+        }
+        key_ranges = new_key_ranges;
+    }
+    for (KeyRangeEntry location_range : key_ranges)
+    {
+        if (location_range.get_min() < min_location)
+        {
+            min_location = location_range.get_min();
+        }
+    }
+
+    return min_location;
+}
+
 int main()
 {
-
-    std::string line;
-    std::list<long> seeds;
-    Map seed_to_soil_map;
-    Map soil_to_fertilizer_map;
-    Map fertilizer_to_water_map;
-    Map water_to_light_map;
-    Map light_to_temp_map;
-    Map temp_to_hum_map;
-    Map hum_to_location_map;
-
-    std::ifstream input_file(INPUT_FILENAME);
     if (!input_file.is_open())
     {
         throw std::runtime_error("Input file is not open");
     }
 
-    std::string seeds_line;
-    std::getline(input_file, seeds_line);
-    parse_seeds(&seeds, seeds_line);
-
-    for (long seed : seeds)
-    {
-        std::cout << seed << std::endl;
-    }
+    // std::cout << "Part 1 Answer: " << part_1() << std::endl;
+    std::cout << "Part 2 Answer: " << part_2() << std::endl;
 }
